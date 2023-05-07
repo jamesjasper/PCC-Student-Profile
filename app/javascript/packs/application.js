478aee07db8483032abb01b43ee770442c6ff976
James Jasper Villamor <jamesjasper.villamor@gmail.com>
2023-05-07 23:45:03 +0800

    fixed webpacker error and added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "channels"
+import 'bootstrap'
+import '../stylesheets/application'
