4357be36dd17f42539eb9bbb38548a88b467bb4b
James Jasper Villamor <jamesjasper.villamor@gmail.com>
2023-05-08 11:30:36 +0800

    Added simple validation to users and students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
-  belongs_to :user
 
-  validates :first_name, :last_name, :middle_name, :phone_number, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence:true
+  validates :middle_name, presence: true
+  validates :course, presence: true
+  validates :year_level, presence: true
+  validates :mother_name, presence: true
+  validates :father_name, presence: true
+  validates :region, presence: true
+  validates :province, presence: true
+  validates :municipality, presence: true
+  validates :barangay, presence: true
+  validates :zipcode, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
-  has_many :students, dependent: :nullify
-  validates :username, presence: true, length: {maximum:20}
+  before_save { self.email = email.downcase! }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 30}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true, length: {minimum: 8}
+
+  validates :username, presence: true, length: {maximum: 20}, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true
+
+
+
+  has_secure_password
 end
