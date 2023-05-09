8baaab64bf4a627f1020c8ae2cbee10c7d8b4215
James Jasper Villamor <jamesjasper.villamor@gmail.com>
2023-05-09 15:28:37 +0800

    fixed navbar layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-require("jquery")
-import "channels"
 import 'bootstrap'
 import '../stylesheets/application'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
