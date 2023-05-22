ebe6efe53d7d0860fabf7b3460f04a760802bf9c
James Jasper Villamor <jamesjasper.villamor@gmail.com>
2023-05-22 09:48:37 +0800

    removed all reference of bootstrap icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap-icons/font/bootstrap-icons.css')
 
 import 'bootstrap'
 import '../stylesheets/application'
